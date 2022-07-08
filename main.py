dc95bc69f0886ef4892b23c5b7d318059dc8e292
Fernando Mavec <fermavec@gmail.com>
2022-07-07 20:19:19 -0500

    request_response_body

--- main.py
+++ main.py
@@ -1,7 +1,25 @@
+#Python
+from typing import Optional
+#Pydantic!
+from pydantic import BaseModel
+#Fast API
 from fastapi import FastAPI
+from fastapi import Body 
+
 
 #Creando variable de ejecucion con instancia de la clase FastAPI
 app = FastAPI()
+
+
+#Models (Heredan de Base Model)
+class Person(BaseModel):
+    #Con tipado estatico
+    first_name: str
+    last_name: str
+    age: int
+    hair_color: Optional[str] = None
+    is_married: Optional[bool] = None
+
 
 #Path operation decorator para ejecución en el home ("/"")
 @app.get("/")
@@ -12,5 +30,5 @@ def home():
 
 #Request and Response Body
 @app.post("/person/new")
-def create_person():
-    pass
+def create_person(person: Person = Body(...)):#Request Body, los ... significan parametro obligartorio
+    return person
